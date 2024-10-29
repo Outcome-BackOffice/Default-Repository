c5e2be52036583a834a8b9006160a2faafff7909
Ubuntu <ubuntu@ip-172-31-8-102.ap-northeast-2.compute.internal>
2024-10-29 07:20:13 +0000

    feat : Default Setting 적용

---
+++ .github/create_labels.py
@@ -0,0 +1,25 @@
+# create_labels.py
+
+import json
+
+import requests
+
+# labels.json 파일을 읽어서 레이블 생성
+with open(".github/labels.json", "r", encoding="utf-8") as file:
+    labels = json.load(file)
+
+repo = "Outcome-BackOffice/test2"
+url = f"https://api.github.com/repos/{repo}/labels"
+headers = {
+    "Authorization": f"token {GITHUB_TOKEN}",
+    "Accept": "application/vnd.github.v3+json",
+}
+
+for label in labels:
+    response = requests.post(url, headers=headers, json=label)
+    if response.status_code == 201:
+        print(f"Label '{label['name']}' created successfully.")
+    elif response.status_code == 422:
+        print(f"Label '{label['name']}' already exists.")
+    else:
+        print(f"Failed to create label '{label['name']}': {response.content}")
