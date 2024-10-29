09cf5ef8c78437dfa2ff215514816042afecaa1c
Ubuntu <ubuntu@ip-172-31-8-102.ap-northeast-2.compute.internal>
2024-10-29 07:47:23 +0000

    feat : 설정 수정

--- .github/create_labels.py
+++ .github/create_labels.py
@@ -11,15 +11,40 @@ headers = {
     "Accept": "application/vnd.github.v3+json",
 }
 
-# labels.json 파일을 읽어서 레이블 생성
-with open(".github/labels.json", "r", encoding="utf-8") as file:
-    labels = json.load(file)
 
-for label in labels:
-    response = requests.post(url, headers=headers, json=label)
-    if response.status_code == 201:
-        print(f"Label '{label['name']}' created successfully.")
-    elif response.status_code == 422:
-        print(f"Label '{label['name']}' already exists.")
+# 기존 레이블 모두 삭제
+def delete_existing_labels():
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        labels = response.json()
+        for label in labels:
+            delete_url = f"{url}/{label['name']}"
+            delete_response = requests.delete(delete_url, headers=headers)
+            if delete_response.status_code == 204:
+                print(f"Label '{label['name']}' deleted successfully.")
+            else:
+                print(
+                    f"Failed to delete label '{label['name']}': {delete_response.content}"
+                )
     else:
-        print(f"Failed to create label '{label['name']}': {response.content}")
+        print(f"Failed to fetch labels: {response.content}")
+
+
+# labels.json 파일을 읽어서 새로운 레이블 생성
+def create_labels():
+    with open(".github/labels.json", "r", encoding="utf-8") as file:
+        labels = json.load(file)
+
+    for label in labels:
+        response = requests.post(url, headers=headers, json=label)
+        if response.status_code == 201:
+            print(f"Label '{label['name']}' created successfully.")
+        elif response.status_code == 422:
+            print(f"Label '{label['name']}' already exists.")
+        else:
+            print(f"Failed to create label '{label['name']}': {response.content}")
+
+
+# 기존 레이블 삭제 및 새 레이블 생성 실행
+delete_existing_labels()
+create_labels()
