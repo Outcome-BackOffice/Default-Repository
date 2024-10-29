f5fcd696afc873234d8ad121f1c5d718e4682adc
Ubuntu <ubuntu@ip-172-31-8-102.ap-northeast-2.compute.internal>
2024-10-29 07:35:04 +0000

    feat : 설정 수정

--- .github/create_labels.py
+++ .github/create_labels.py
@@ -3,19 +3,17 @@ import os
 
 import requests
 
-# 환경 변수에서 GitHub 토큰 불러오기
-GITHUB_TOKEN = os.environ["GITHUB_TOKEN"]
+# 환경 변수에서 현재 레포지토리 이름과 GitHub 토큰 가져오기
+repo = os.environ["GITHUB_REPOSITORY"]
+url = f"https://api.github.com/repos/{repo}/labels"
+headers = {
+    "Authorization": f"token {os.environ['GITHUB_TOKEN']}",
+    "Accept": "application/vnd.github.v3+json",
+}
 
 # labels.json 파일을 읽어서 레이블 생성
 with open(".github/labels.json", "r", encoding="utf-8") as file:
     labels = json.load(file)
-
-repo = "Outcome-BackOffice/test2"
-url = f"https://api.github.com/repos/{repo}/labels"
-headers = {
-    "Authorization": f"token {GITHUB_TOKEN}",
-    "Accept": "application/vnd.github.v3+json",
-}
 
 for label in labels:
     response = requests.post(url, headers=headers, json=label)
